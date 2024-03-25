b01f70fc6b10e9c6fb5db96d2996be4c6e6f143d
Akshaykumar Sharma <96808759+yahskamrahs@users.noreply.github.com>
2024-03-25 18:59:23 +0530

    Update usersData.js

--- usersData.js
+++ usersData.js
@@ -1,40 +1,42 @@
 var usersData = {
-  "user1": {
-    "password": "pass123",
-    "name": "John Doe",
-    "mobile": "1234567890",
-    "address": "123 Main St, City",
-    "wallet_balance": 1000,
+  "D50001602": {
+    "password": "Bahatre",
+    "name": "Dhammapal Bahatre",
+    "mobile": "+919359694398",
+    "address": "At jaroda,post ghoda jaroda,Ghoda,Hingoli,Maharashtra-431702",
+    "wallet_balance": "17,142.74",
+    "holding_int":"98,938.00",
+    "mrk_value":"30,000.00",
     "stocks": [
       {
-        "Sector":"INT INITIAL",
-        "token": "ABC123",
-        "value": 534,
-        "status": "active"
+        "Sector":"INT INITIALS",
+        "token": "122",
+        "value": "67,211.51",
+        "status": "SLOT CLOSED"
       },
       {
-        "Sector":"INT INITIAL",
-        "token": "ABC123",
-        "value": 500,
-        "status": "active"
+        "Sector":"INT INITIALS",
+        "token": "126",
+        "value": "40,186.71",
+        "status": "SLOT CLOSED"
       },
       {
-        "Sector":"INT INITIAL",
-        "token": "ABC123",
-        "value": 500,
-        "status": "active"
+        "Sector":"INT INITIALS",
+        "token": "187",
+        "value": "1,10,000.00",
+        "status": "SLOT CLOSED"
       },
       {
-        "Sector":"INT INITIAL",
-        "token": "ABC123",
-        "value": 500,
-        "status": "active"
+        "Sector":"INT INITIALS",
+        "token": "190",
+        "value": "45,000",
+        "status": "SLOT ACTIVE"
       },
       {
-        "Sector":"INT INITIAL",
-        "token": "ABC123",
-        "value": 500,
-        "status": "active"
+        "Sector":"INT TRADE SECTOR",
+        "token": "UD",
+        "value": "30,000",
+        "status": "SECTOR ACTIVE"
       }
       
     ],
@@ -46,6 +48,8 @@ var usersData = {
     "mobile": "9876543210",
     "address": "456 Elm St, Town",
     "wallet_balance": 2000,
+    "holding_int":3000,
+    "mrk_value":5000,
     "stocks": [
       {
         "Sector":"INT INITIAl",
@@ -67,6 +71,8 @@ var usersData = {
     "mobile": "5555555555",
     "address": "789 Oak St, Village",
     "wallet_balance": 5000,
+    "holding_int":3000,
+    "mrk_value":5000,
     "stocks": [
       {
         "Sector":"INT INITIAl",
